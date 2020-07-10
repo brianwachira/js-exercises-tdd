49ca333b83999f63502ddf288c5078f4f8cbb467
brianwachira <brianwachira7@gmail.com>
2020-07-10 12:41:24 +0300

    add function to pass test

--- I.pass-tests/01-add-one/add-one.js
+++ I.pass-tests/01-add-one/add-one.js
@@ -1,1 +1,11 @@
-module.exports = function(numbers) {};
+function addOne(numbers) {
+
+  const numbersPlusOne = numbers.map(function(number){
+
+    return number+1
+  });
+
+  return numbersPlusOne;
+};
+
+module.exports = addOne;
