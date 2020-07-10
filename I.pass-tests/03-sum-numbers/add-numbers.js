6b7dfcfee5812d1a263a4c15bb891b28ee30c6f8
brianwachira <brianwachira7@gmail.com>
2020-07-10 13:10:24 +0300

    add function definition to pass test

--- I.pass-tests/03-sum-numbers/add-numbers.js
+++ I.pass-tests/03-sum-numbers/add-numbers.js
@@ -1,2 +1,12 @@
-function addNumbers(numbers) {}
+function addNumbers(numbers) {
+
+let sumOfNumbers = 0;
+
+numbers.forEach(function(number){
+
+  sumOfNumbers+=number;
+});
+
+return sumOfNumbers;
+}
 module.exports = addNumbers;
