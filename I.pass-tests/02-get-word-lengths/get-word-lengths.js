b51fb7297f8c1644a70fb4bc790405fff9a4aa9a
brianwachira <brianwachira7@gmail.com>
2020-07-10 12:47:03 +0300

    add function definition to pass test

--- I.pass-tests/02-get-word-lengths/get-word-lengths.js
+++ I.pass-tests/02-get-word-lengths/get-word-lengths.js
@@ -1,3 +1,11 @@
-var getWordLengths = function(someWords) {};
+var getWordLengths = function(someWords) {
+
+  const wordLengths = someWords.map(function(someWord){
+
+    return someWord.length;
+  });
+
+  return wordLengths;
+};
 
 module.exports = getWordLengths;
