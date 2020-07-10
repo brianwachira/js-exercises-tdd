4b3808926b3c5b1e10e61c3eca7c92045bb264d6
brianwachira <brianwachira7@gmail.com>
2020-07-10 13:10:42 +0300

    add function definition to pass test

--- I.pass-tests/04-find-needle/find-needle.js
+++ I.pass-tests/04-find-needle/find-needle.js
@@ -1,3 +1,11 @@
-function findNeedle(words) {}
+function findNeedle(words,term) {
+
+
+  for (let index = 0; index < words.length; index++){
+    if(words[index].includes(term)){
+      return index;
+    }
+  }
+}
 
 module.exports = findNeedle;
